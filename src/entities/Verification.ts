f3ee0c278aedae85d9931926be0046fc444a7827
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-08-17 08:34:00 +0900

    Verification에 랜덤 key값 생성해주는 코드 작성

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -5,14 +5,18 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
+  BeforeInsert,
 } from "typeorm";
 import { verificationTarget } from "../types/types";
+
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
 
 @Entity()
 class Verification extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+  @Column({ type: "text", enum: [PHONE, EMAIL] })
   target: verificationTarget;
 
   @Column({ type: "text" })
@@ -26,5 +30,15 @@ class Verification extends BaseEntity {
 
   @CreateDateColumn() createdAt: string;
   @UpdateDateColumn() updatedAt: string;
+
+  // Insert되기 전에 key값을 생성해주는 코드
+  @BeforeInsert()
+  createKey(): void {
+    if (this.target === PHONE) {
+      this.key = Math.floor(Math.random() * 100000).toString();
+    } else if (this.target === EMAIL) {
+      this.key = Math.random().toString(36).substr(2);
+    }
+  }
 }
 export default Verification;
