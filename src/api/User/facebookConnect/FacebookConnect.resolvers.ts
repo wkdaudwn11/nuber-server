53f737e97b9a7bc4af61483fb7e38b85fbd513e7
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-08-23 22:28:01 +0900

    Facebook 로그인 및 회원가입 Resolver 작성

---
+++ src/api/User/facebookConnect/FacebookConnect.resolvers.ts
@@ -0,0 +1,49 @@
+import User from "../../../entities/User";
+import {
+  FacebookConnectMutationArgs,
+  FacebookConnectResponse,
+} from "./../../../types/graph.d";
+import { Resolvers } from "./../../../types/resolvers.d";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    FacebookConnect: async (
+      _,
+      args: FacebookConnectMutationArgs
+    ): Promise<FacebookConnectResponse> => {
+      const { fbId } = args;
+
+      try {
+        const existingUser = await User.findOne({ fbId });
+
+        if (existingUser) {
+          // 기존 유저
+          return {
+            ok: true,
+            error: null,
+            token: "Coming soon",
+          };
+        } else {
+          // 새로운 유저
+          await User.create({
+            ...args,
+            profilePhoto: `http://graph.facebook.com/${fbId}/picture?type=square`,
+          }).save();
+          return {
+            ok: true,
+            error: null,
+            token: "Coming soon",
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null,
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
