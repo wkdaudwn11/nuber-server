486d5ab375ab1d46ec59977b786d514d24702900
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-09-15 22:07:21 +0900

    StartPhoneVerification Resolver #2 (핸드폰 문자 인증)

---
+++ src/utils/sendSMS.ts
@@ -0,0 +1,11 @@
+// import Twilio from "twilio";
+
+// const twilioClient = Twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+export const sendSMS = (to: string, message: string) => {
+  console.log("to >", to);
+  console.log("message >", message);
+};
+
+export const sendVerificationSMS = (to: string, key: string) =>
+  sendSMS(to, `인증번호: ${key}`);
