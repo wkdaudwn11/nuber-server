a0630ad83412515f136eaf67661fc2e082f90758
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-09-21 20:07:01 +0900

    UpdateMyProfile Resolver 작성 #2

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -1,14 +1,38 @@
 import User from "../../../entities/User";
-import { UpdateMyProfileMutationArgs } from "../../../types/graph";
+import {
+  UpdateMyProfileMutationArgs,
+  UpdateMyProfileResponse,
+} from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
   Mutation: {
     UpdateMyProfile: privateResolver(
-      async (_, args: UpdateMyProfileMutationArgs, { req }) => {
-        const user: User = req.user;
-        await User.update({ id: user.id }, { ...args });
+      async (
+        _,
+        args: UpdateMyProfileMutationArgs,
+        { req }
+      ): Promise<UpdateMyProfileResponse> => {
+        try {
+          const user: User = req.user;
+          const newUser = {};
+          Object.keys(args).forEach((key) => {
+            if (args[key] !== null) {
+              newUser[key] = args[key];
+            }
+          });
+          await User.update({ id: user.id }, { ...newUser });
+          return {
+            ok: true,
+            error: null,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+          };
+        }
       }
     ),
   },
