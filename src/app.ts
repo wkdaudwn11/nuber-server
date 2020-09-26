761d58368aeffd808faca6c49be13e4e0b425653
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-09-26 10:24:25 +0900

    DriversSubscription (Graphql-yoga의 pubSub을 이용한 구독 기능) #1

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import { NextFunction, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
@@ -8,17 +8,23 @@ import decodeJWT from "./utils/decodeJWT";
 
 class App {
   public app: GraphQLServer;
+  public pubSub: any;
+
   constructor() {
+    this.pubSub = new PubSub();
+    this.pubSub.ee.setMaxListeners(99);
     this.app = new GraphQLServer({
       schema,
       context: (req) => {
         return {
           req: req.request,
+          pubSub: this.pubSub,
         };
       },
     });
     this.middleWares();
   }
+
   private middleWares = (): void => {
     this.app.express.use(cors());
     this.app.express.use(logger("dev"));
