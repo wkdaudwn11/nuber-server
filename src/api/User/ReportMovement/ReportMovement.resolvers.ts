21b6af3bea0cabb128da82c9a948b47bfdf02cf9
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-09-26 10:36:57 +0900

    DriversSubscription (Graphql-yoga의 pubSub을 이용한 구독 기능) #2

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -13,12 +13,18 @@ const resolvers: Resolvers = {
       async (
         _,
         args: ReportMovementMutationArgs,
-        { req }
+        { req, pubSub }
       ): Promise<ReportMovementResponse> => {
         const user: User = req.user;
         const notNull = cleanNullArgs(args);
         try {
           await User.update({ id: user.id }, { ...notNull });
+          /**
+           * 드라이버의 위치가 바뀌었을 때 driverUpdate 방에 계속 정보를 push 해주는 거라고 생각하면 될듯
+           * driverUpdate라는 방은 일반 사용자들이 드라이버를 찾기 위해서 구독중일테고
+           * 그 방에다가 나 드라이버에요~ 라고 알려주기 위해서 명함 내미는 것임.
+           */
+          pubSub.publish("driverUpdate", { DriversSubscription: user });
           return {
             ok: true,
             error: null,
