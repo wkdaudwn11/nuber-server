02180282987d9ee1a3e8c800d5cf1e93b18bdbe3
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-09-26 10:11:40 +0900

    GetNearbyDrivers Resolver 작성 (근처 운전자 찾기) #2

--- src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
+++ src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
@@ -1,4 +1,4 @@
-import { Between } from "typeorm";
+import { Between, getRepository } from "typeorm";
 import User from "../../../entities/User";
 import { GetNearbyDriversResponse } from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
@@ -10,13 +10,28 @@ const resolvers: Resolvers = {
       async (_, __, { req }): Promise<GetNearbyDriversResponse> => {
         const user: User = req.user;
         const { lastLat, lastLng } = user;
-
-        // typeorm의 Between을 이용하여 현재 유저의 위도, 경도가 +-0.05인 운전자를 찾음 (근처 운전자를 찾음)
-        const drivers = await User.find({
-          isDriving: true,
-          lastLat: Between(lastLat - 0.05, lastLat + 0.05),
-          lastLng: Between(lastLng - 0.05, lastLng + 0.05),
-        });
+        try {
+          /**
+           * typeorm의 Between을 이용하여 현재 유저의 위도, 경도가 +-0.05인 운전자를 찾음 (근처 운전자를 찾음)
+           * https://github.com/typeorm/typeorm
+           */
+          const drivers: User[] = await getRepository(User).find({
+            isDriving: true,
+            lastLat: Between(lastLat - 0.05, lastLat + 0.05),
+            lastLng: Between(lastLng - 0.05, lastLng + 0.05),
+          });
+          return {
+            ok: true,
+            error: null,
+            drivers,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            drivers: null,
+          };
+        }
       }
     ),
   },
