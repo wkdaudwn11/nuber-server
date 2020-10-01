ef6dfa827fe0d62565a079a05facb89a7045833b
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-10-01 15:05:14 +0900

    Authenticating WebSocket Subscriptions #1 (구독 하기 전에 인증된 회원인지 검증)

--- src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
+++ src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
@@ -5,7 +5,7 @@ const resolvers = {
         /**
          * 어떤 유저가 근처의 드라이버를 찾기 위해서 driverUpdate 라는 채팅방에 입장하는 거라고 이해하는 게 편함
          * driverUpdate 라는 방에는 현재 내 위치랑 가까운 드라이버들의 정보가 있음
-         * 그걸 보기 위해서 방에 입장아흔? 거라고 생각하는 게 좀 더 이해가 편할듯함
+         * 그걸 보기 위해서 방에 입장하는? 거라고 생각하는 게 좀 더 이해가 편할듯함
          */
         return pubSub.asyncIterator("driverUpdate");
       },
--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,38 @@ import { Options } from "graphql-yoga";
 import { createConnection } from "typeorm";
 import app from "./app";
 import connectionOptions from "./ormConfig";
+import decodeJWT from "./utils/decodeJWT";
 
 // graphql-yoga의 options 변수들
 const PORT: number | string = process.env.PORT || 4000; // 포트
 const PLAYGROUND_ENDPOINT: string = "/playground"; // playground 주소
 const GRAPHQL_ENDPOINT: string = "/graphql"; // graphql api 주소
+const SUBSCRIPTION_ENDPOINT: string = "/subscriptions"; // 구독 관련 주소 (socket 연결 주소)
 
 // app의 옵션
 const appOptions: Options = {
   port: PORT,
   playground: PLAYGROUND_ENDPOINT,
   endpoint: GRAPHQL_ENDPOINT,
+  subscriptions: {
+    path: SUBSCRIPTION_ENDPOINT,
+    onConnect: async (connectionParams) => {
+      /**
+       * onConnect()는 socket이 연결 될 때, 즉 구독을 할 때 실행되는 함수
+       * 이때 인증된 회원인지 검사한다. (JWT 토큰 검사)
+       */
+      const token = connectionParams["X-JWT"];
+      if (token) {
+        const user = await decodeJWT(token);
+        if (user) {
+          return {
+            currentUser: user,
+          };
+        }
+      }
+      throw new Error("인증되지 않은 회원이므로 구독 할 수 없습니다.");
+    },
+  },
 };
 
 // app의 callback 함수
